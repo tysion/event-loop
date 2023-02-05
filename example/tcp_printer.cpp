ff6a89380f4bb63bf04607d8b7f43f194bb5702d
Tikhon Sergienko <t.sergienko@visionlabs.ru>
2023-02-05 16:08:06 +0300

    make example more clear

--- example/tcp_printer.cpp
+++ example/tcp_printer.cpp
@@ -1,101 +1,125 @@
-#include <iostream>
-#include <oxm/event_loop.h>
-
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <oxm/event_loop.h>
 
-const int MAX_EVENTS = 10;
+#include <utility>
 
-int createSocket() {
-  int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_sock < 0) {
-    perror("[ERROR:1] Socket cannot be created!");
-    exit(EXIT_FAILURE);
-  }
+using EventLoopPtr = std::shared_ptr<oxm::EventLoop>;
 
-  return listen_sock;
-}
+struct TcpAcceptor {
+  explicit TcpAcceptor(EventLoopPtr loop, int listen_socket)
+      : loop_{std::move(loop)}, listen_socket_{listen_socket} {
+    oxm::Event accept_event;
+    accept_event.TriggerOn(oxm::Event::Read);
+    accept_event.fd = listen_socket_;
 
-void listen(int socket) {
-  sockaddr_in server_addr = {
-      .sin_family = AF_INET,
-      .sin_port = htons(9000)
-  };
-
-  inet_pton(AF_INET, "0.0.0.0", &server_addr.sin_addr);
-
-  if (bind(socket, (sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    perror("[ERROR:2] Socket cannot be created!");
-    exit(EXIT_FAILURE);
-  }
-
-  if (listen(socket, SOMAXCONN) < 0) {
-    perror("[ERROR:3] Socket cannot be switched to listen mode!");
-    exit(EXIT_FAILURE);
-  }
-}
-
-int main() {
-  auto eventloop = std::make_shared<oxm::EventLoop>();
-
-  int listen_sock = createSocket();
-  listen(listen_sock);
-
-
-  auto event = oxm::Event();
-  event.TriggerOn(oxm::Event::Read);
-  event.fd = listen_sock;
-
-  auto listenEventId = eventloop->RegisterEvent(event);
-
-  auto task1 = eventloop->CreateTask([eventloop, listen_sock, listenEventId](oxm::Event::Mask mask) {
-    sockaddr_in client_addr;
-    socklen_t client_addr_size = sizeof(client_addr);
-    int conn_sock = accept(listen_sock, (sockaddr *)&client_addr, &client_addr_size);
-    if (conn_sock == -1) {
-      perror("[ERROR:7] accept");
-      exit(EXIT_FAILURE);
-    }
-
-    if (fcntl(conn_sock, F_SETFL, O_NONBLOCK) == -1) {
-      perror("[ERROR:8] fcntl");
-      exit(EXIT_FAILURE);
-    }
-
-    auto event = oxm::Event();
-    event.TriggerOn(oxm::Event::Read);
-    event.fd = conn_sock;
-
-    auto eventId = eventloop->RegisterEvent(event);
-
-    auto task2 = eventloop->CreateTask([eventloop, conn_sock, eventId](oxm::Event::Mask mask) {
+    accept_event_id_ = loop_->RegisterEvent(accept_event);
+    oxm::TaskPtr on_accept = loop_->CreateTask([this](oxm::Event::Mask mask) {
       if (oxm::HasError(mask)) {
-        perror("[ERROR:9] Error");
-        eventloop->Unshedule(eventId, true);
+        HandleError(mask, accept_event_id_);
         return;
       }
 
-      if (oxm::CanRead(mask)) {
-        char buffer[4096];
-        int len = read(conn_sock, &buffer, 4096);
-        if (len > 0) {
-          write(1, buffer, len);
-        }
-      }
+      const int connection_socket = Accept();
+      HandleConnection(connection_socket);
     });
 
-    eventloop->Bind(eventId, task2);
-    eventloop->Schedule(eventId);
-  });
+    loop_->Bind(accept_event_id_, on_accept);
+  }
 
-  eventloop->Bind(listenEventId, task1);
-  eventloop->Schedule(listenEventId);
+  void AcceptAsync() {
+    loop_->Schedule(accept_event_id_);
+  }
+
+ private:
+  void HandleConnection(int connection_socket) {
+    oxm::Event connection_event;
+    connection_event.fd = connection_socket;
+    connection_event.TriggerOn(oxm::Event::Read);
+
+    oxm::Event::Id connection_event_id = loop_->RegisterEvent(connection_event);
+    oxm::TaskPtr on_connect =
+        loop_->CreateTask([this, connection_event_id, connection_socket](oxm::Event::Mask mask) {
+          if (oxm::HasError(mask)) {
+            HandleError(mask, connection_event_id);
+            return;
+          }
+
+          if (oxm::CanRead(mask)) {
+            char buffer[4096];
+            auto len = read(connection_socket, &buffer, 4096);
+            if (len > 0) {
+              write(1, buffer, len);
+            }
+          }
+        });
+
+    loop_->Bind(connection_event_id, on_connect);
+    loop_->Schedule(connection_event_id);
+  }
+
+  void HandleError(oxm::Event::Mask mask, oxm::Event::Id id) {
+    if (mask & oxm::Event::RemoteConnectionClosed) {
+      printf("Error: Remote connection was closed");
+    } else if (mask & oxm::Event::FileDescriptorError) {
+      printf("Error: EPOLL: File descriptor error");
+    } else {
+      printf("Error: Unknown error");
+    }
+
+    loop_->Unshedule(id, true);
+  }
+
+  int Accept() const {
+    sockaddr_in address = {};
+    socklen_t address_size = sizeof(address);
+
+    int connection_socket = accept(listen_socket_, (sockaddr*)&address, &address_size);
+    if (connection_socket == -1) {
+      throw std::runtime_error("Accept failed");
+    }
+
+    if (fcntl(connection_socket, F_SETFL, O_NONBLOCK) == -1) {
+      throw std::runtime_error("Make non blocking failed");
+    }
+
+    return connection_socket;
+  }
+
+  const int listen_socket_;
+  oxm::Event::Id accept_event_id_;
+  EventLoopPtr loop_;
+};
+
+TcpAcceptor Listen(EventLoopPtr loop, const char* address, int port) {
+  int listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_socket < 0) {
+    throw std::runtime_error("Socket cannot be created!");
+  }
+
+  sockaddr_in listen_address = {.sin_family = AF_INET, .sin_port = htons(port)};
+
+  inet_pton(AF_INET, address, &listen_address.sin_addr);
+
+  if (bind(listen_socket, (sockaddr*)&listen_address, sizeof(listen_address)) < 0) {
+    throw std::runtime_error("Socket cannot be created!");
+  }
+
+  if (listen(listen_socket, SOMAXCONN) < 0) {
+    throw std::runtime_error("Socket cannot be switched to listen mode!");
+  }
+
+  return TcpAcceptor(std::move(loop), listen_socket);
+}
+
+int main() {
+  auto loop = std::make_shared<oxm::EventLoop>();
+
+  auto acceptor = Listen(loop, "0.0.0.0", 9000);
+
+  acceptor.AcceptAsync();
 
   while (true) {
-    eventloop->Poll();
+    loop->Poll();
   }
 }
