1e788181f0b5f4f6e9ebf933bac41bb044ec1b1d
Tikhon <tihonsergienko@yandex.ru>
2023-03-07 01:51:13 +0300

    multithreaded test

--- test/lock_free_queue_test.cpp
+++ test/lock_free_queue_test.cpp
@@ -16,7 +16,7 @@ TEST_CASE("Single thread tests", kTag) {
     REQUIRE_FALSE(queue.TryPop(elem));
   }
 
-  SECTION("Pop removes elem from queue") {
+  SECTION("TryPop removes elem from queue") {
     int elem = 0;
     REQUIRE(queue.TryPush(1));
     REQUIRE(queue.TryPop(elem));
@@ -37,4 +37,47 @@ TEST_CASE("Single thread tests", kTag) {
     }
     REQUIRE_FALSE(queue.TryPop(elem));
   }
+}
+
+TEST_CASE("Multiple threads", kTag) {
+  const size_t size = 1024 * 128;
+  oxm::LockFreeQueue<int> queue(size);
+
+  std::thread producer_1([&]() {
+    int i = 0;
+    while (i < size) {
+      if (queue.TryPush(i)) {
+        i += 2;
+      }
+    }
+  });
+
+  std::thread producer_2([&]() {
+    int i = 1;
+    while (i < size) {
+      if (queue.TryPush(i)) {
+        i += 2;
+      }
+    }
+  });
+
+  std::thread consumer([&]() {
+    int counter = 0;
+    int value;
+    std::vector<bool> bitset(size, false);
+
+    while (counter < size) {
+      if (queue.TryPop(value)) {
+        REQUIRE(bitset[value] == false);
+        bitset[value] = true;
+        ++counter;
+      }
+    }
+
+    REQUIRE(std::all_of(bitset.begin(), bitset.end(), [](bool res) { return res; }));
+  });
+
+  producer_1.join();
+  producer_2.join();
+  consumer.join();
 }
